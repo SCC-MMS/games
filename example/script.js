7f18e34a5379b6497c4afac9ff2f9aa2c198bed4
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2025-01-14 21:58:54 +0000

    files for tuesday, jan 14

---
+++ example/script.js
@@ -0,0 +1,1 @@
+<html>    <head>        <title>[Name]'s Game</title>        <link href="style.css" rel="stylesheet" />        <script src="https://console.shuchir.dev/inject.js"></script>        <script src="script.js"></script>    </head>    <body>        <!-- All HTML goes here -->    </body><!DOCTYPE html></html>
